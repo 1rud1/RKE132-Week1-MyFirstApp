0d27e398cb34fd83a61a9be0435e8639a286d0b8
Rudolf Bender <rudolf.bender@tktk.ee>
2024-02-11 15:23:59 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//This is a comment
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Hello, Rudolf!"); //output
+//string - sõne
+string userName = Console.ReadLine(); //input
+
+
+Console.WriteLine("Hello" + "," + userName + "!"); //output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); //output
